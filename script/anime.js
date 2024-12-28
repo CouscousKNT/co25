f697b9675b012d09aa245847aaf25f0987f0f212
Ousmane <ousmaneknt@outlook.fr>
2024-12-29 00:24:17 +0100

    Add files

---
+++ script/anime.js
@@ -0,0 +1,62 @@
+const canvas = document.querySelector(".canvas");
+if (document.querySelector(".canvas")) {
+  console.log(document.querySelector(".canvas"));
+}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+const frameCount = 26;
+
+const currentFrame = (index) => `./images/scroll/${(index + 1).toString()}.jpg`;
+
+const images = [];
+let ball = { frame: 0 };
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  console.log(currentFrame(i));
+  images.push(img);
+}
+
+gsap.to(ball, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: "power1.out",
+  scrollTrigger: {
+    scrub: 0.9,
+    pin: "canvas",
+    end: "150%",
+  },
+  onUpdate: render,
+});
+
+gsap.fromTo(
+  ".ball-text",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    scrollTrigger: {
+      scrub: 1,
+
+      start: "50%",
+      end: "60%",
+    },
+    onComplete: () => {
+      gsap.to(".ball-text", { opacity: 0 });
+    },
+  }
+);
+
+images[0].onload = render;
+
+function render() {
+  context.canvas.width = images[0].width;
+  context.canvas.height = images[0].height;
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[ball.frame], 0, 0);
+}
